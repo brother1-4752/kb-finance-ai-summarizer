b426091e08e4db0f07cf7713458d20ccae355538
brother1-4752 <guddlf4752@gmail.com>
2023-09-04 23:53:20 +0900

    feat: react-pdf 관련 패키지 설치 및 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,9 @@ import Head from 'next/head'
 import globalStyles from '@/styles/globalStyles'
 import Layout from '@/components/Layout/Layout'
 import theme from '@/styles/theme'
+import { pdfjs } from 'react-pdf'
 
-
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -15,12 +16,12 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>KB Finance AI Summarizer</title>
       </Head>
 
-        <ThemeProvider theme={theme}>
-          <Global styles={globalStyles} />
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <Global styles={globalStyles} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
     </>
   )
 }
